59a72b7ab57d14a483b911b69a2a52bdba053d9e
Sammie <lythekiet333@gmail.com>
2023-08-04 13:56:22 +0930

    Initialize the project MVC
    Initialize the project MVC

---
+++ AOWebApp/Models/CodeFirst/ExampleItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AOWebApp.Models.CodeFirst
+{
+    public class ExampleItem
+    {
+        [Key]
+        public int itemID { get; set; }
+        [Required]
+        [Display(Name = "Item Name")]
+        [StringLength(100, ErrorMessage ="The item name must be less than 100 characters")]
+        public string itemName { get; set; }
+
+
+        [Required(ErrorMessage ="You must provide an item price")]
+        [Range(1, 10000, ErrorMessage ="The {0} must be between {1} and {2}")]
+        [Display(Name ="Price")]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal itemPrice { get; set; }
+    }
+}
