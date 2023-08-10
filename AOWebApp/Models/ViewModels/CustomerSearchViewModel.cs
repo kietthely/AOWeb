067baa109223720efdcbcec619ca21796d4eec0d
Sammie <lythekiet333@gmail.com>
2023-08-10 16:55:12 +0930

    Create CustomerSearchViewModel
    Create CustomerSearchViewModel

---
+++ AOWebApp/Models/ViewModels/CustomerSearchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using AOWebApp.Models;
+using AOWebApp.Helpers;
+namespace AOWebApp.Models.ViewModels
+{
+    public class CustomerSearchViewModel
+    {
+        public string  SearchText{ get; set; }
+        public string Suburb { get; set; }
+        public SelectList SuburbList{ get; set; }
+        public List<Customer> CustomerList{ get; set; }
+
+        public List<string> NameList { get; set; }
+    }
+}
