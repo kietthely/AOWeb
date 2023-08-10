565456b5a212c811237f17eeae689dd2f4618d94
Sammie <lythekiet333@gmail.com>
2023-08-10 15:05:12 +0930

    Added Item_ItemDetail model
    Added Item_ItemDetail model to display the item instead of ItemSearch model

---
+++ AOWebApp/Models/ViewModels/Item_ItemDetail.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AOWebApp.Models.ViewModels
+{
+    public class Item_ItemDetail
+    {
+        public Item TheItem { get; set; }
+        public int ReviewCount{ get; set; }
+        public double AverageRating { get; set; }
+
+    }
+}
