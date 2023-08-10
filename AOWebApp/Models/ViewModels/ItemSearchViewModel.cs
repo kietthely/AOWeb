53008b3cb0c838c4d4a0a1d058e289b52d8357d2
Sammie <lythekiet333@gmail.com>
2023-08-10 15:05:25 +0930

    Added ItemSearchViewModel
    Added ItemSearchViewModel

---
+++ AOWebApp/Models/ViewModels/ItemSearchViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using AOWebApp.Models;
+using AOWebApp.Helpers;
+
+namespace AOWebApp.Models.ViewModels
+{
+    public class ItemSearchViewModel
+    {
+
+        public string? SearchText { get; set; }
+        public int? CategoryId { get; set; }
+        public SelectList? CategoryList { get; set; }
+        public PaginatedList<Item_ItemDetail>? ItemList { get; set; }
+
+    }
+}
