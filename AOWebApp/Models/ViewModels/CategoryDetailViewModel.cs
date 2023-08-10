fe9d52b746202db2913d942edf9fe54884435ccb
Sammie <lythekiet333@gmail.com>
2023-08-10 15:04:42 +0930

    Added CategoryDetail model
    Added CategoryDetail model for future use

---
+++ AOWebApp/Models/ViewModels/CategoryDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AOWebApp.Models.ViewModels
+{
+    public class CategoryDetailViewModel
+    {
+        [Required]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
